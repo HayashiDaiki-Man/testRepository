91539ab56e5e84094efed3c873d7dae73e4eaa4b
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-06-06 07:09:42 +0900

    add
    テキストデータの読み込み・書き込み

--- testfile/src/App.java
+++ testfile/src/App.java
@@ -1,5 +1,17 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        FileReader fw = new FileReader("C:/java/testRepository/testfile/src/sample.txt");
+        int i = fw.read();
+
+        while (i != -1) {
+            char c = (char) i;
+            System.out.println(c);
+            i = fw.read();
+        }
+        System.out.println("おわり");
+        fw.close();
     }
 }
